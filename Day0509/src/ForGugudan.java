22364c94ecfda39700a4a07c968164cc41f93bbc
ShinMyeongseop28 <167171248+ShinMyeongseop28@users.noreply.github.com>
2024-05-10 17:45:38 +0900

    For 구구단

--- Day0509/src/ForGugudan.java
+++ Day0509/src/ForGugudan.java
@@ -5,7 +5,7 @@ public class ForGugudan {
 		//구구단 7단 출력하기
 		//7 X 1 = 7
 		//7 X 9 = 63
-		int dan = 7;
+		int dan = 6;
 		for(int by=1; by<=9; by++) {
 			System.out.printf("%d X %d = %d\n", dan, by, dan*by);
 		}
