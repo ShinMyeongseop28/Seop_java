113e84366916edb7bf9c59423e81101691b1a21a
ShinMyeongseop28 <167171248+ShinMyeongseop28@users.noreply.github.com>
2024-05-19 23:34:28 +0900

    두 정수간 사칙연산 메소드

--- Day0516/src/MethodEx02.java
+++ Day0516/src/MethodEx02.java
@@ -8,22 +8,24 @@ public class MethodEx02 {
 		System.out.println("곱셈 result: " + result);
 		//곱셈 연산 결과값을 나눗셈 연산의 피젯수로 사용해야 한다: 10(피젯수) / 2(젯수)
 		double dResult = divide(result, n3);
-		System.out.println("나눗셈 result: " + result);
+		System.out.println("나눗셈 result: " + dResult);
 	}
+	
 	//두 정수간의 사칙연산을 하는 메소드
 	//덧셈연산
-	static int add(int no1, int no2) {
+	static void add(int no1, int no2) {
 		int result = no1 + no2;
 		System.out.printf("%d + %d = %d\n", no1, no2, result);
-		return result; //return할 데이터가 없는 경우 생략가능
+		return; //return할 데이터가 없는 경우 생략가능
 	}
+	
 	//뺄셈연산
 	static int minus(int no1, int no2) {
 		int result = no1 - no2;
 		System.out.printf("%d - %d = %d\n", no1, no2, result);
-		return result; 
-		
+		return result;
 	}
+	
 	//곱셈연산
 	static int multiply(int no1, int no2) {
 		int result = no1 * no2;
@@ -34,7 +36,7 @@ public class MethodEx02 {
 	//나눗셈연산
 	static double divide(int no1, int no2) {
 		double result = no1 / (double)no2;
-		System.out.printf("%d / %d = %f\n", no1, no2, result);
+		System.out.printf("%d / %d = %.1f\n", no1, no2, result);
 		return result;
 	}
 
