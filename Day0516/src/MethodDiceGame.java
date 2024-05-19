914ad23fe5fd585ca583a2da1ca821f7c8f3ce00
ShinMyeongseop28 <167171248+ShinMyeongseop28@users.noreply.github.com>
2024-05-19 23:35:06 +0900

    메소드 주사위 게임

--- Day0516/src/MethodDiceGame.java
+++ Day0516/src/MethodDiceGame.java
@@ -1,56 +1,47 @@
 import java.util.Random;
 
 public class MethodDiceGame {
-
 	
-	static void throwDice() {
+	static int throwDice(int player, String name) {
 		Random r = new Random();
-		int dice1, dice2, no=0;
-		int hong=0, park=0; //각 게임자의 이동 최종위치를 담을 변수
-		while(true) {
-			
-			System.out.printf("[%d회차] 주사위를 던지세요~\n", ++no);
-			
-			//홍길동
+		int dice1, dice2;
+		
 			do {
 				dice1 = r.nextInt(6) + 1;
 				dice2 = r.nextInt(6) + 1;
-				hong += dice1 + dice2;
+				player += dice1 + dice2;
 				System.out.printf("%s: (%d,%d) -> %d칸 이동 ==> 최종위치: %d\n",
-						"홍길동", dice1, dice2, dice1+dice2, hong);
+						name, dice1, dice2, dice1+dice2, player);
 				if(dice1==dice2) {
-					System.out.println("홍길동 한번 더!");
+					System.out.printf("%s 한번 더!\n", name);
 				}
 			}while(dice1==dice2);
+			
+			return player;
 	}
 	
 	public static void main(String[] args) {
-		//주사위 던지기 메소드
 		
+		Random r = new Random();
+		int no = 0;
+		int hong = 0, park = 0;
+		while(true) {
 			
-			//박문수
-			do {
-				dice1 = r.nextInt(6) + 1;
-				dice2 = r.nextInt(6) + 1;
-				player +=  dice1 + dice2;
-				System.out.printf("%s: (%d,%d) -> %d칸 이동 ==> 최종위치: %d\n",
-						"박문수", dice1, dice2, dice1+dice2, park);
-				if(dice1==dice2) {
-					System.out.println("박문수 한번 더!");
-				}
-			}while(dice1==dice2);
+			System.out.printf("[%d회차] 주사위를 던지세요~\n", ++no);
 			//홍길동/박문수의 최종위치가 50이상이면 게임끝, 둘 다 50이상이 되면 무승부
+			hong = throwDice(hong, "홍길동");
+			park = throwDice(park, "박문수");
 			if(hong>=50 && park>=50) {
 				System.out.println("무승부");
 				break;
 			}else if(hong>=50 || park>=50) {
 				System.out.println("게임끝: "
-						+ (hong>=50 ? "홍길동" : "박문수"));
+						+ (hong>=50 ? "홍길동 승" : "박문수 승"));
 				break;
 			}
 			
 		}
-
+			
 	}
 
 }
