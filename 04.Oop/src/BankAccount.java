72a57594fa6228154ba8176589d562e19160d7d3
ShinMyeongseop28 <dkrntm2714@gmail.com>
2024-05-23 17:46:45 +0900

    은행계좌 클래스

--- 04.Oop/src/BankAccount.java
+++ 04.Oop/src/BankAccount.java
@@ -5,16 +5,18 @@ public class BankAccount {
 		//메소드: 입금한다(입금한 금액만큼 잔액을 증가시키기)
 				//출금한다(입금한 금액만큼 잔액을 감소시키기, 출금한 금액을 반환하기)
 	
-	String accountNumber, name;
-	int amount;
+	String accountNumber, owner;
+	int balance;
 	
-	void inputMoney(int money) {
-		amount += money; 
+	void deposit(int money) {
+		balance += money;
 	}
 	
-	int outputMoney(int money) {
-		amount -= money;
-		return amount;
+	int withdraw(int money) {
+		//통장 잔고보다 출금액이 많으면 출금은 안됨
+		if(balance < money) return 0;
+		balance -= money;
+		return money;
 	}
 
 }
--- 04.Oop/src/BankAccountMain.java
+++ 04.Oop/src/BankAccountMain.java
@@ -7,11 +7,60 @@ public class BankAccountMain {
 				//통장정보: 예금주, 계좌번호, 잔액을 출력
 				//5만원 입금한 후 출력
 				//3만원 출금한 후 출력
+		BankAccount hong = new BankAccount();
+		hong.owner = "홍길동";
+		hong.accountNumber = "1111-222-3333";
 		
+		//받아든 통장의 정보확인
+		System.out.printf("%s의 통장--------\n", hong.owner);
+		System.out.printf("계좌번호: %s\n", hong.accountNumber);
+		System.out.printf("잔액: %d\n", hong.balance);
 		
+		//며칠후: 5만원 입금한 후 출력
+		hong.deposit(50000);
+		accountInfo(hong);
 		
+		//며칠후: 3만원 출금한 후 출력
+		hong.withdraw(30000);
+		accountInfo(hong);
 		
+		//박문수씨 통장개설
+		BankAccount park = new BankAccount();
+		park.owner = "박문수";
+		park.accountNumber = "22-333-4444";
+		park.balance = 50000;
+		
+		//통장 정보 확인
+		accountInfo(park);
+		
+		//박씨가 3만원 입금
+		park.deposit(30000);
+		accountInfo(park);
+		
+		//박씨가 10만원 출금
+		int request = 100000;
+//		int hand = park.withdraw(request);
+//		accountInfo(park, request, hand);
+		accountInfo(park, request, park.withdraw(request));
 
+		//		int request = 100000;
+//		int output = park.withdraw( request );
+//		accountInfo( park, request, output );
+		
 	}
+	
+	static void accountInfo(BankAccount who) {
+		System.out.printf("%s의 통장--------\n", who.owner);
+		System.out.printf("계좌번호: %s\n", who.accountNumber);
+		System.out.printf("잔액: %d\n", who.balance);
+	}
+	
+	static void accountInfo(BankAccount who, int request, int hand) {
+		System.out.printf("%s의 통장--------\n", who.owner);
+		System.out.printf("계좌번호: %s\n", who.accountNumber);
+		System.out.printf("출금 요청액: " + request + "\n출금된 금액: " + hand);
+		System.out.printf("\n잔액: %d\n", who.balance);
+	}
+	
 
 }
