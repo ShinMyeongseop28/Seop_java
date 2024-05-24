cddd294cd05c2d9e4d122ac979e7a85106f21c9c
ShinMyeongseop28 <dkrntm2714@gmail.com>
2024-05-24 18:04:08 +0900

    상품정보 생성자

---
+++ 04.Oop/src/Day0524/Product.java
@@ -0,0 +1,22 @@
+package Day0524;
+
+public class Product {
+	//1. 상품정보 클래스 Product 선언
+	//1-1)필드: 상품명, 상품코드, 상품가격
+	String ProductName, ProductCode; 
+	int Price;
+	//1-2)생성자: 상품명, 상품코드를 초기화, 모든필드를 초기화
+	Product(String ProductName, String ProductCode) {
+		this.ProductName = ProductName;
+		this.ProductCode = ProductCode;
+	}
+	Product(String ProductName, String ProductCode, int Price) {
+		this(ProductName, ProductCode);
+		this.Price = Price;
+	}
+	//1-3)메소드: 가격을 변경한다.
+	void changePrice(int Price) {
+		this.Price = Price;
+	}
+	
+}
